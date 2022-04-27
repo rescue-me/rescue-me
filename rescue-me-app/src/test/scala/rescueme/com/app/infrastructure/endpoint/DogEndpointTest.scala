a15ca263a2e4b246d32d76996c1fe062f10bdbf0
alejandro <atorresato@gmail.com>
2022-04-27 15:22:59 +0200

    test for dog endpoint by id

--- rescue-me-app/src/test/scala/rescueme/com/app/infrastructure/endpoint/DogEndpointTest.scala
+++ rescue-me-app/src/test/scala/rescueme/com/app/infrastructure/endpoint/DogEndpointTest.scala
@@ -8,7 +8,8 @@ import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.dsl.Http4sDsl
 import org.http4s.implicits._
 import org.http4s.server.Router
-import org.http4s.{EntityDecoder, EntityEncoder, HttpApp}
+import org.http4s.{EntityDecoder, EntityEncoder, HttpApp, Uri}
+import org.scalatest.OptionValues
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
@@ -23,7 +24,8 @@ class DogEndpointTest
     with Http4sDsl[IO]
     with Http4sClientDsl[IO]
     with Arbitraries
-    with ScalaCheckPropertyChecks {
+    with ScalaCheckPropertyChecks
+    with OptionValues {
 
   implicit val entityDecoder: EntityDecoder[IO, Dog] = jsonOf[IO, Dog]
   implicit val entityEncoder: EntityEncoder[IO, Dog] = jsonEncoderOf[IO, Dog]
@@ -46,6 +48,20 @@ class DogEndpointTest
         body.size should be > 0
       }).unsafeRunSync()
     }
+  }
 
+  test("Should create and retrieve by id") {
+    forAll { dog: Dog =>
+      (for {
+        createReq  <- POST(dog, uri"/dogs")
+        createRes  <- router.run(createReq)
+        createdDog <- createRes.as[Dog]
+        req        <- GET(Uri.unsafeFromString(s"/dogs/${createdDog.id.value}"))
+        resp       <- router.run(req)
+        body       <- resp.as[Dog]
+      } yield {
+        body shouldBe createdDog
+      }).unsafeRunSync()
+    }
   }
 }
