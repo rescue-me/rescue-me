9bdbfc22d19c47c8063f20cbfe0fe0c0c8b4cb0f
alejandro <atorresato@gmail.com>
2022-05-13 17:34:04 +0200

    adapter stub modified following signature

--- rescue-me-app/src/test/scala/rescueme/com/app/infrastructure/repository/DogDetailStubRepository.scala
+++ rescue-me-app/src/test/scala/rescueme/com/app/infrastructure/repository/DogDetailStubRepository.scala
@@ -5,6 +5,7 @@ import rescueme.com.app.domain.Identifier
 import rescueme.com.app.domain.dog.{DogDetail, DogDetailRepositoryAlgebra}
 
 object DogDetailStubRepository extends DogDetailRepositoryAlgebra[IO] {
-  override def getDetails(id: Identifier): IO[List[DogDetail]]                          = IO.pure(List())
-  override def updateDetails(id: Identifier, details: DogDetail): IO[Option[DogDetail]] = IO.pure(None)
+  override def getDetails(id: Identifier): IO[Option[DogDetail]]        = IO.pure(None)
+  override def updateDetails(details: DogDetail): IO[Option[DogDetail]] = IO.pure(None)
+  override def createDetails(detail: DogDetail): IO[DogDetail]          = ???
 }
